686f1f3d80cec4d0bf33c64a07d8e63ce954c29e
mlstudyguru <90481005+mlstudyguru@users.noreply.github.com>
2021-10-04 07:32:39 +0800

    Update assignment.py

--- hw1/code/assignment.py
+++ hw1/code/assignment.py
@@ -81,7 +81,7 @@ class Model:
         Y = np.eye(self.batch_size)[labels, 0:self.num_classes]
         Z = (Y - probabilities) / self.batch_size
         gradW =  np.dot(inputs.T, Z)  
-        gradB =  self.learning_rate* np.sum(Z)
+        gradB =  np.sum(Z)
              
         return gradW, gradB
         pass
@@ -109,7 +109,7 @@ class Model:
         '''
         # TODO: change the weights and biases of the model to descent the gradient
         self.W += gradW * self.learning_rate
-        self.b += gradB 
+        self.b += gradB * self.learning_rate
         pass
     
 def train(model, train_inputs, train_labels):
